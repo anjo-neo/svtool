605e831a6b4fe53da3fb842252851be935a2a417
niclas-dotcom <82120644+niclas-dotcom@users.noreply.github.com>
2021-04-13 12:33:20 +0200

    Create angemeldet.php
    Wenn der Benutzer angemeldet ist

---
+++ angemeldet.php
@@ -0,0 +1,56 @@
+<html lang=de>
+    <head>
+    <title>Startseite</title>
+        <meta charset="utf-8">
+        
+    </head>
+    <body>
+
+       <?php 
+        $id=$_POST['benutzer'];
+        $pw=$_POST['pw'];
+        $passwort=0;
+        
+        
+     $myDbServer="127.0.0.1";//standard ip für localhost
+    $myDbName="umfrage";
+    $myDbLogin="root"; //root
+    $myDbLoginPW="";
+
+    
+    try {
+    $dbPDO = new PDO('mysql:host='.$myDbServer.';dbname='.$myDbName, $myDbLogin, $myDbLoginPW);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+        $anmeld=$dbPDO->query("SELECT pwd FROM pw WHERE b_id=$id");
+    
+    
+   
+    
+    foreach($anmeld as $row){
+        $passwort=$row['pwd'];
+    }
+    
+ 
+        if($_POST['pw']==$passwort){   
+          echo "<p>angemeldet</p>";
+ 
+ }
+    
+    if($_POST['pw']!=$passwort){
+        
+        echo "<p> falsche Eingabe, versuche es erneut</p>";
+        
+    }
+
+  //-------------schliessen DB Verbindung---------------
+  $dbPDO=NULL;
+
+        
+        
+        
+    ?>
+    
+    </body>
+</html>
