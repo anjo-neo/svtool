c048a4fab76e56f8f82f592ee092de8845ce7503
niclas-dotcom <82120644+niclas-dotcom@users.noreply.github.com>
2021-04-13 12:10:50 +0200

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -2,8 +2,14 @@ CREATE DATABASE umfrage;
 USE umfrage;
 CREATE TABLE benutzer(
 b_id int(5) NOT NULL AUTO_INCREMENT,
-pwd int(5) NOT NULL,
 rechte char(1) NOT NULL,
 name VARCHAR(20) NOT NULL,
 PRIMARY KEY(b_id)
 );
+
+CREATE TABLE pw(
+  b_id int(5) NOT NULL,
+  pwd int(5) NOT NULL,
+  PRIMARY KEY(b_id),
+  FOREIGN KEY(b_id) REFERENCES benutzer(b_id)
+  );
