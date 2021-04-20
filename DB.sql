75f5d61e014dc42e1f75a181ad668d41e4920602
niclas-dotcom <82120644+niclas-dotcom@users.noreply.github.com>
2021-04-20 11:25:14 +0200

    Update DB.sql
    Hier ist die vorläufige Erweiterung der DB, kann und muss wahrscheinlich noch verbessert werden

--- DB.sql
+++ DB.sql
@@ -14,5 +14,32 @@ CREATE TABLE pw(
   FOREIGN KEY(b_id) REFERENCES benutzer(b_id)
   );
   
+CREATE TABLE treffen(
+    t_id int(5) NOT NULL AUTO_INCREMENT,
+    datum DATE NOT NULL,
+    PRIMARY KEY(t_id)
+    );
+    
+    CREATE TABLE umfragen(
+        u_id int(5) NOT NULL AUTO_INCREMENT,
+        fragetext VARCHAR(40) NOT NULL,
+        titel VARCHAR(20) NOT NULL,
+        treffen int(5) NOT NULL,
+        ja int(3) NOT NULL,
+        nein int(3)NOT NULL,
+        vielleicht int(3) NOT NULL,
+        PRIMARY KEY(u_id),
+        FOREIGN KEY(treffen) REFERENCES treffen(t_id)
+        );
+        
+        CREATE TABLE anwesenheit(
+            a_id int(5) NOT NULL AUTO_INCREMENT,
+            treffen int(5) NOT NULL,
+            teilnehmer int(5) NOT NULL,
+            PRIMARY KEY(a_id),
+            FOREIGN KEY(treffen) REFERENCES treffen(t_id),
+            FOREIGN KEY(teilnehmer) REFERENCES benutzer(b_id)
+            );
+  
   INSERT INTO benutzer(rechte,name) VALUES("a","n");
 INSERT INTO pw(b_id,pwd) VALUES(1,12345);
